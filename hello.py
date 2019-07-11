1fb2ed69c27d85fbe1872df2097c6451c70b8436
Davelmk <i417528032@163.com>
2019-07-11 14:33:24 +0800

    init

---
+++ hello.py
@@ -0,0 +1,17 @@
+import turtle
+
+print('hello python')
+
+# 单行注释
+
+'''
+多行注释
+'''
+
+print('hello','world',sep=', ',end='!')
+print('hello world',end='!\n')
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
